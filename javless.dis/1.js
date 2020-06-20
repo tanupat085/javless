fe8c5235aa63b309dbfe3493d4a847a8d39c9b45
tanupat085 <54698281+tanupat085@users.noreply.github.com>
2020-06-20 21:50:55 +0700

    java lesson

---
+++ javless.dis/1.js
@@ -0,0 +1,57 @@
+//ตัวแปร var  global  , let  local 
+var a = 10 ;
+
+// (() => {
+// function run(){
+//     console.log(a);
+// }
+// run();
+// })();
+// ---------------------------------------
+// (() => {
+// var a = 50 ;
+// var s = 60 ;
+// var d = a +  s ;
+// var q = 60 ;
+// var r = 70 ;
+// var e = q + r ;
+// var t = "เทส" ;
+// console.log(d + e);
+// console.log(t) ;
+// })();
+
+// (() => {
+// var i = 5 ;
+// var o = 6 ;
+// var u = i + o ;
+
+// var a = 5 ;
+// var b = 9 ;
+// var c = a + b ;
+
+// async function sa(){
+//     await console.log(u);
+//     ++u;
+//     if ( u > 50 ){
+//         u = 0 ;
+//     }
+//     u ? 
+// }
+// setInterval(() => sa(), 100);
+
+
+// })() ;
+
+
+var grade = 4 ;
+grade = 73;
+
+if (grade  >= 4 ){
+    console.log('grade ' + grade);  
+}
+if(grade ===   4) {
+    console.log('sa');
+}
+
+
+
